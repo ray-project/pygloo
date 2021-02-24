18a0862660ec2ef0e01330288897da1de6e73298
Hao Zhang <zhisbug@users.noreply.github.com>
2021-02-23 19:39:13 -0500

    fix a build problem for manylinux2014 wheel (#2)

--- build_wheels.sh
+++ build_wheels.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Assuming in docker "quay.io/pypa/manylinux_2_24_x86_64".
+# Assuming in docker "quay.io/pypa/manylinux2014_x86_64".
 
 # Build py36 manylinux wheels
 PYTHON_BIN_PATH='/opt/python/cp36-cp36m/bin/python' /opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
-auditwheel repair --plat manylinux_2_24_x86_64 dist/pygloo-0.1.0-cp36-cp36m-linux_x86_64.whl
+auditwheel repair --plat manylinux2014_x86_64 dist/pygloo-0.1.0-cp36-cp36m-linux_x86_64.whl
 bazel clean --expunge
 /opt/python/cp36-cp36m/bin/python setup.py clean --all
 
 # Build py37 manylinux wheels
 PYTHON_BIN_PATH='/opt/python/cp37-cp37m/bin/python' /opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
-auditwheel repair --plat manylinux_2_24_x86_64 dist/pygloo-0.1.0-cp37-cp37m-linux_x86_64.whl
+auditwheel repair --plat manylinux2014_x86_64 dist/pygloo-0.1.0-cp37-cp37m-linux_x86_64.whl
 bazel clean --expunge
 /opt/python/cp37-cp37m/bin/python setup.py clean --all
 
 # Build py38 manylinux wheels
 PYTHON_BIN_PATH='/opt/python/cp38-cp38/bin/python' /opt/python/cp38-cp38/bin/python setup.py bdist_wheel
-auditwheel repair --plat manylinux_2_24_x86_64 dist/pygloo-0.1.0-cp38-cp38-linux_x86_64.whl
+auditwheel repair --plat manylinux2014_x86_64 dist/pygloo-0.1.0-cp38-cp38-linux_x86_64.whl
 bazel clean --expunge
 /opt/python/cp38-cp38/bin/python setup.py clean --all
