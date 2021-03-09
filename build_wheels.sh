d02482f699a61aa17837a196494875fac0d1212c
Hao Zhang <zhisbug@users.noreply.github.com>
2021-03-09 17:44:01 -0500

    upldate the wheel generation script (#7)

--- build_wheels.sh
+++ build_wheels.sh
@@ -4,18 +4,18 @@
 
 # Build py36 manylinux wheels
 PYTHON_BIN_PATH='/opt/python/cp36-cp36m/bin/python' /opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
-auditwheel repair --plat manylinux2014_x86_64 dist/pygloo-0.1.0-cp36-cp36m-linux_x86_64.whl
+auditwheel repair --plat manylinux2014_x86_64 dist/pygloo-0.2.0-cp36-cp36m-linux_x86_64.whl
 bazel clean --expunge
 /opt/python/cp36-cp36m/bin/python setup.py clean --all
 
 # Build py37 manylinux wheels
 PYTHON_BIN_PATH='/opt/python/cp37-cp37m/bin/python' /opt/python/cp37-cp37m/bin/python setup.py bdist_wheel
-auditwheel repair --plat manylinux2014_x86_64 dist/pygloo-0.1.0-cp37-cp37m-linux_x86_64.whl
+auditwheel repair --plat manylinux2014_x86_64 dist/pygloo-0.2.0-cp37-cp37m-linux_x86_64.whl
 bazel clean --expunge
 /opt/python/cp37-cp37m/bin/python setup.py clean --all
 
 # Build py38 manylinux wheels
 PYTHON_BIN_PATH='/opt/python/cp38-cp38/bin/python' /opt/python/cp38-cp38/bin/python setup.py bdist_wheel
-auditwheel repair --plat manylinux2014_x86_64 dist/pygloo-0.1.0-cp38-cp38-linux_x86_64.whl
+auditwheel repair --plat manylinux2014_x86_64 dist/pygloo-0.2.0-cp38-cp38-linux_x86_64.whl
 bazel clean --expunge
 /opt/python/cp38-cp38/bin/python setup.py clean --all
